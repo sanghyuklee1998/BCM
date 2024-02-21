8eeea3f1ee8810157827c43b5840aec1fa8b5d6b
qordpsem <qkrrb1223@naver.com>
2024-02-21 19:31:34 +0900

    bugfix#41 Post, Member, Comment에 modelNotFoundException 적용 안되는 버그 수정

--- src/main/kotlin/com/example/bcm/domain/global/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/bcm/domain/global/exception/GlobalExceptionHandler.kt
@@ -2,7 +2,6 @@ package com.example.bcm.domain.global.exception
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.ErrorResponse
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
