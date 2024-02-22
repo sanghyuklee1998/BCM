c7d7162e5c3d0bd87cf6e3d5372ad3eef5176599
sanghyuklee1998 <lsh980726@naver.com>
2024-02-23 00:03:46 +0900

    cache를 빌드에 추가했고 cachemanager도 추가했습니다

---
+++ src/main/kotlin/com/example/bcm/domain/post/repository/PostRepositoryV2.kt
@@ -0,0 +1,19 @@
+package com.example.bcm.domain.post.repository
+
+import com.example.bcm.domain.post.model.Post
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PostRepositoryV2: JpaRepository<Post, Long> {
+//    fun findByTitleLike(title: String, pageable: Pageable): Page<Post>
+//    fun findByContentLike(content: String, pageable: Pageable): Page<Post>
+
+    @Cacheable(value = ["postByTitleORContent"], key = "''")
+    fun findByTitleContainsOrContentContains(title: String, content: String, pageable:Pageable): Page<Post>
+    fun findAllByOrderByCreatedAtDesc(pageable: Pageable): Page<Post>
+
+}
